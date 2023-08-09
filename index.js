7468a2f1533ab5c003a8b48b65c323d73e25fa1c
Kevin H <110433246+kevinx9000@users.noreply.github.com>
2023-08-09 14:50:55 -0700

    code formatting

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
 let passwordLength = 15
 
 function getCharacters1() {
     let password1 = ""
     for (let i = 0; i < passwordLength; i++) {
-        let randomNumber1 = Math.floor( Math.random() * characters.length )
+        let randomNumber1 = Math.floor(Math.random() * characters.length)
         randomCharacter = characters[randomNumber1]
         password1 += randomCharacter
-        }
-        return password1
+    }
+    return password1
 }
 
 function getCharacters2() {
     let password2 = ""
     for (let i = 0; i < passwordLength; i++) {
-        let randomNumber2 = Math.floor( Math.random() * characters.length )
+        let randomNumber2 = Math.floor(Math.random() * characters.length)
         randomCharacter = characters[randomNumber2]
         password2 += randomCharacter
-        }
-        return password2
+    }
+    return password2
 }
 
 let passwordEl1 = document.getElementById("pw-1")
